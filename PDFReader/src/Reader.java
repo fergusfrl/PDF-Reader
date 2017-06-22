eccce2d478e6258465c0ea0b1184d6969a6310fd
Fergus Farrell <fergusfrl@gmail.com>
2017-06-22 20:38:58 +1200

    Add files via upload

--- PDFReader/src/Reader.java
+++ PDFReader/src/Reader.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
@@ -8,14 +10,14 @@ import org.apache.pdfbox.text.PDFTextStripperByArea;
 
 public class Reader {
 
+	public static HashMap<String, Integer> wordCount = new HashMap<>();
+	
 	public static void main(String[] args) throws InvalidPasswordException, IOException {
 		
 		ArrayList<PDDocument> documents = new ArrayList<>();
-		documents.add(PDDocument.load(new File("2016 Exam.pdf")));
-		documents.add(PDDocument.load(new File("2015 Exam.pdf")));
-		documents.add(PDDocument.load(new File("2014 Exam.pdf")));
-		documents.add(PDDocument.load(new File("2013 Exam.pdf")));
-		documents.add(PDDocument.load(new File("2012 Exam.pdf")));
+		documents.add(PDDocument.load(new File("test1.pdf")));
+		documents.add(PDDocument.load(new File("test2.pdf")));
+		documents.add(PDDocument.load(new File("test3.pdf")));
 		
 		try{
 			for(int i = 0; i < documents.size(); i++){
@@ -26,30 +28,32 @@ public class Reader {
 					st.setSortByPosition(true);
 					PDFTextStripper Tstripper = new PDFTextStripper();
 					String str = Tstripper.getText(doc);
-					splitWords(str);
-					//countWords();
+					mapWords(str);
 				}
 			}
 		} catch (IOException e){
 			System.out.println(e.getMessage());
 		}
+		System.out.println(wordCount);
+		printWords(wordCount);
 	}
 	
-	public static void splitWords(String input) throws FileNotFoundException{
+	public static void mapWords(String input) throws FileNotFoundException{
 		String[] words = input.split("\\s+");
-		printToTextFile(words);
+		for(int i = 0; i < words.length; i++){
+			if(wordCount.containsKey(words[i])){
+				wordCount.put(words[i], wordCount.get(words[i])+1);
+			} else {
+				wordCount.put(words[i], 1);
+			}
+		}
 	}
 	
-	public static void printToTextFile(String[] words){
-		try(FileWriter fw = new FileWriter("test.txt", true);
-			    BufferedWriter bw = new BufferedWriter(fw);
-			    PrintWriter out = new PrintWriter(bw))
-			{
-			   for(int i = 0; i < words.length; i++){
-				   out.println(words[i]);
+	public static void printWords(HashMap<String, Integer> wordCount){
+		for(int i = Collections.max(wordCount.values()); i > 0; i--){
+			if(wordCount.containsValue(i)){
+				System.out.println(i);
 			}
-		} catch (IOException e) {
-			    System.out.print(e);
 		}
 	}
 	
