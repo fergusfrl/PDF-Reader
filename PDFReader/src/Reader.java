c2de7307308560af15f8c0180df3b5df24b13b5e
Fergus Farrell <fergusfrl@gmail.com>
2017-06-22 20:22:26 +1200

    Add files via upload
    Creates HashMap of words(key) and frequency(value)

--- PDFReader/src/Reader.java
+++ PDFReader/src/Reader.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -10,15 +11,14 @@ import org.apache.pdfbox.text.PDFTextStripperByArea;
 public class Reader {
 
 	public static HashMap<String, Integer> wordCount = new HashMap<>();
+	public static HashMap<Integer, String> printable = new HashMap<>();
 	
 	public static void main(String[] args) throws InvalidPasswordException, IOException {
 		
 		ArrayList<PDDocument> documents = new ArrayList<>();
-		documents.add(PDDocument.load(new File("2016 Exam.pdf")));
-		documents.add(PDDocument.load(new File("2015 Exam.pdf")));
-		documents.add(PDDocument.load(new File("2014 Exam.pdf")));
-		documents.add(PDDocument.load(new File("2013 Exam.pdf")));
-		documents.add(PDDocument.load(new File("2012 Exam.pdf")));
+		documents.add(PDDocument.load(new File("test1.pdf")));
+		documents.add(PDDocument.load(new File("test2.pdf")));
+		documents.add(PDDocument.load(new File("test3.pdf")));
 		
 		try{
 			for(int i = 0; i < documents.size(); i++){
@@ -30,33 +30,18 @@ public class Reader {
 					PDFTextStripper Tstripper = new PDFTextStripper();
 					String str = Tstripper.getText(doc);
 					splitWords(str);
-					//countWords();
-					System.out.println(wordCount);
 				}
 			}
 		} catch (IOException e){
 			System.out.println(e.getMessage());
 		}
+		printWords(wordCount);
 	}
 	
 	public static void splitWords(String input) throws FileNotFoundException{
 		String[] words = input.split("\\s+");
-		//printToTextFile(words);
 		countWords(words);
 	}
-	
-	//public static void printToTextFile(String[] words){
-	//	try(FileWriter fw = new FileWriter("test.txt", true);
-	//		    BufferedWriter bw = new BufferedWriter(fw);
-	//		    PrintWriter out = new PrintWriter(bw))
-	//		{
-	//		   for(int i = 0; i < words.length; i++){
-	//			   out.println(words[i]);
-	//		}
-	//	} catch (IOException e) {
-	//		    System.out.print(e);
-	//	}
-	//}
 	
 	public static void countWords(String[] words){
 		for(int i = 0; i < words.length; i++){
@@ -64,6 +49,15 @@ public class Reader {
 				wordCount.put(words[i], wordCount.get(words[i])+1);
 			} else {
 				wordCount.put(words[i], 1);
+			}
+		}
+	}
+	
+	public static void printWords(HashMap<String, Integer> wordCount){
+		System.out.println(wordCount);
+		for(int i = Collections.max(wordCount.values()); i > 0; i--){
+			if(wordCount.containsValue(i)){
+				System.out.println(i);
 			}
 		}
 	}
